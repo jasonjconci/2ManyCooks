0708379b7f80f604a7bc5b0b16f59a3ab2a732e9
JasonJConci <jasonjconci@gmail.com>
2018-12-08 16:03:01 -0800

    Updated app.py to use different buttons

--- app.py
+++ app.py
@@ -9,14 +9,16 @@ def index():
     if request.method == "GET":
         pass
     elif request.method == "POST":
-        recipe_name = str(request.form.get('name'))
-        cook_time = int(request.form.get('cook_time'))
-        difficulty = int(request.form.get('difficulty'))
-        rating = int(request.form.get('rating'))
-        m.create_recipe(recipe_name, cook_time, difficulty, rating)
-
+        if request.form['submit_button'] == "Search":
+            pass
+        elif request.form['submit_button'] == "Submit":
+            recipe_name = str(request.form.get('name'))
+            cook_time = int(request.form.get('cook_time'))
+            difficulty = int(request.form.get('difficulty'))
+            rating = int(request.form.get('rating'))
+            m.create_recipe(recipe_name, cook_time, difficulty, rating)
     recipes = m.get_all_recipes()
-    return render_template('index.html', recipes=m.get_recipe_with_id(4))
+    return render_template('index.html', recipes=recipes)
     
 if __name__ == "__main__":
     app.run(debug=True)
